50ae49cf8cf2035e0bf1d14837984c72e0d23f04
SchneiderellaBee <barbara.psch@gmail.com>
2024-04-22 03:02:19 -0300

    feat: primeiro mapa com pontos teste

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -1,17 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.component.html',
-  styleUrls: ['./mapa.component.scss']
+  styleUrls: ['./mapa.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class MapaComponent implements OnInit {
-  
-  display: google.maps.LatLngLiteral|null = null;
-  center: google.maps.LatLngLiteral = {lat: 24, lng: 12};
-  zoom = 4;
-  
+
+  mapOptions: google.maps.MapOptions = {
+    center: { lat: 38.9987208, lng: -77.2538699 },
+    zoom: 14,
+    disableDefaultUI: true,
+    zoomControl: false,
+    mapTypeControl: false,
+    streetViewControl: false,
+    fullscreenControl: false,
+    backgroundColor: 'none',
+  };
+
+  display: google.maps.LatLngLiteral | null = null;
+  center: google.maps.LatLngLiteral = { lat: -29.1681, lng: -51.1792 }; // Coordenadas de Caxias do Sul
+  zoom = 13;
+
+  icons: Record<string, { icon: string }> = {
+    point1: {
+      icon: 'assets/icone_ecoponto1.png'
+    },
+    point2: {
+      icon: 'assets/icone_ecoponto2.png'
+    }
+  };
+
+  markers: google.maps.LatLngLiteral[] = [
+    { lat: -29.167, lng: -51.179 }, // Coordenadas primeiro ponto
+    { lat: -29.170, lng: -51.181 }  // Coordenadas segundo ponto
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
@@ -19,13 +45,7 @@ export class MapaComponent implements OnInit {
 
   moveMap(event: google.maps.MapMouseEvent) {
     if (event.latLng) {
-      this.center = (event.latLng?.toJSON());
-    }
-  }
-
-  move(event: google.maps.MapMouseEvent) {
-    if (event.latLng) {
-      this.display = event.latLng?.toJSON();
+      this.center = event.latLng.toJSON();
     }
   }
 
