09c993b2465169aa3969f95441f8bdd1f51dbd5b
SchneiderellaBee <barbara.psch@gmail.com>
2024-05-13 18:54:58 -0300

    fix: mock lat and lng ptos map

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -30,12 +30,32 @@ export class MapaComponent implements OnInit {
     },
     point2: {
       icon: 'assets/icone_ecoponto2.png'
+    },
+    point3: {
+      icon: 'assets/icone_ecoponto2.png'
+    },
+    point4: {
+      icon: 'assets/icone_ecoponto2.png'
+    },
+    point5: {
+      icon: 'assets/icone_ecoponto2.png'
+    },
+    point6: {
+      icon: 'assets/icone_ecoponto2.png'
+    },
+    point7: {
+      icon: 'assets/icone_ecoponto2.png'
     }
   };
 
   markers: google.maps.LatLngLiteral[] = [
-    { lat: -29.167, lng: -51.179 }, // Coordenadas primeiro ponto
-    { lat: -29.170, lng: -51.181 }  // Coordenadas segundo ponto
+    { lat: -29.167, lng: -51.179 },
+    { lat: -29.170, lng: -51.181 },
+    { lat: -29.17110, lng: -51.14016 },
+    { lat: -29.17140, lng: -51.16764 },
+    { lat: -29.16642, lng: -51.16557 },
+    { lat: -29.16657, lng: -51.18574 },
+    { lat: -29.13374, lng: -51.19270 }
   ];
 
   constructor() { }
