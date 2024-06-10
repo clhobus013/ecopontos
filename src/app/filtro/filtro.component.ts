46db87a4559f32efa29030123f92d1333b896f00
claudia <clauu013@gmail.com>
2024-06-09 23:01:11 -0300

    Busca lista de residuos da api

--- src/app/filtro/filtro.component.ts
+++ src/app/filtro/filtro.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { EcopontoService } from '../services/ecoponto.service';
+import { Residuo } from '../models/residuo';
+import { IconName } from '@fortawesome/fontawesome-common-types';
+import { Categoria } from '../models/categoria';
 
 @Component({
   selector: 'app-filtro',
@@ -7,16 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FiltroComponent implements OnInit {
 
-  residuos: String[] = [];
+  residuos: Residuo[] = [];
 
-  constructor() { }
+  constructor(private ecopontoService: EcopontoService) { }
 
   ngOnInit(): void {
-    this.residuos = [
-      "Esponja de cozinha",
-      "Pilha",
-      "Cápsula de café",
-    ]
+    this.buscarResiduos();
+  }
+
+  public async buscarResiduos() {
+    await this.ecopontoService.getResiduos()
+      .subscribe(
+        (data: any) => {
+          this.residuos = data.values.map((residuo: any) => 
+              new Residuo({
+                id: residuo.id,
+                descricao: residuo.descricao,
+                icone: residuo.icone as IconName,
+                urlMidia: residuo.url_midia,
+                recolhidoEcoponto: residuo.recolhido_em_ecoponto,
+                ativo: false,
+                categorias: residuo.categoria.map((categ: any) => new Categoria({descricao: categ.descricao, icone: categ.icone}))
+              })
+          );
+        },
+        (error: any) => {
+          console.log(error);
+        }
+      );
   }
 
 }
