cbc76a6a5bc60fe6333b89ca0e65822c7a37978a
claudia <clauu013@gmail.com>
2024-06-23 17:01:57 -0300

    Altera nome environment, agora o correta

--- mynode.js
+++ mynode.js
@@ -2,19 +2,19 @@ const fs = require('fs');
 const path = require('path');
 const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
-const dotenv = require('dotenv').config({path: 'src/.env'}); ;
+const dotenv = require('dotenv').config({path: 'src/.env'});
 
 const envFile = `export const environment = {
     production: '${process.env.production}',
     apiUrl: '${process.env.apiUrl}',
 };
 `;
-const targetPath = path.join(__dirname, './src/environments/environment.ts');
+const targetPath = path.join(__dirname, './src/environments/environment.prod.ts');
 fs.writeFile(targetPath, envFile, (err) => {
     if (err) {
         console.error(err);
         throw err;
     } else {
-        console.log(successColor, `${checkSign} Successfully generated environment.ts`);
+        console.log(successColor, `${checkSign} Successfully generated environment.prod.ts`);
     }
 });
